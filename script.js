f609f03f747356f3bf4aedd079056997f3b46a6c
Tristan <84967423+tnjnck@users.noreply.github.com>
2024-03-06 15:22:49 +0000

    Add search non-workspace drive

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function openDriveSearch() {
     const driveIds = document.getElementById('driveIds').value.split(',');
     const searchTerm = document.getElementById('searchTerm').value;
+    const searchAll = document.getElementById('searchAll').checked;
     const searchBaseUrl = 'https://drive.google.com/drive/u/0/search?q=';
+
+    if (searchAll) {
+        // Open a tab to search across all Google Drive (without 'in:')
+        window.open(`${searchBaseUrl}${encodeURIComponent(searchTerm)}`, '_blank');
+    }
 
     driveIds.forEach(id => {
         const trimmedId = id.trim();
@@ -11,14 +17,18 @@ function openDriveSearch() {
         }
     });
 
-    // Save the last used IDs in local storage
+    // Save states in local storage
     localStorage.setItem('lastUsedDriveIds', driveIds);
+    localStorage.setItem('searchAllChecked', searchAll);
 }
 
-// Load last used IDs on page load
+// Load last used states on page load
 document.addEventListener('DOMContentLoaded', () => {
     const lastUsedDriveIds = localStorage.getItem('lastUsedDriveIds');
+    const searchAllChecked = localStorage.getItem('searchAllChecked') === 'true';
+
     if (lastUsedDriveIds) {
         document.getElementById('driveIds').value = lastUsedDriveIds;
     }
+    document.getElementById('searchAll').checked = searchAllChecked;
 });
